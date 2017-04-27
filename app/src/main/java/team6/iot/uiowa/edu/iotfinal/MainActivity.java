7be1c1511ff7d72181347b58ff253a375e5a8197
Luke Meyer <luke@bellwethermade.com>
2017-04-27 16:29:21 -0500

    db should work now

--- app/src/main/java/team6/iot/uiowa/edu/iotfinal/MainActivity.java
+++ app/src/main/java/team6/iot/uiowa/edu/iotfinal/MainActivity.java
@@ -7,34 +7,45 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.*;
-import com.google.firebase.storage.*;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    private StorageReference mStorageRef;
+    private DatabaseReference mDataRef;
     private FirebaseAuth mAuth;
+    private static final String TAG = "AnonAuth";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDataRef = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
-        mStorageRef = FirebaseStorage.getInstance().getReference();
-        System.out.println(mStorageRef);
     }
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        System.out.println(currentUser);
+        updateUI(currentUser);
+        signInAnon();
+    }
+
+    private void signInAnon () {
         mAuth.signInAnonymously()
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -43,11 +54,11 @@ public class MainActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInAnonymously:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-//                            updateUI(user);
+                            updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInAnonymously:failure", task.getException());
-                            Toast.makeText(AnonymousAuthActivity.this, "Authentication failed.",
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                             updateUI(null);
                         }
@@ -57,31 +68,40 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-
     public void testFunction(View view){
-        byte[] bytes = "hello".getBytes();
-        System.out.println(bytes);
-        StorageReference byteRef = mStorageRef.child("hello/bytes");
-        System.out.println(byteRef);
-        byteRef.putBytes(bytes);
+        mDataRef.push().setValue("test");
+
+//        System.out.println(byteRef);
+//        UploadTask uploadTask = byteRef.putBytes(bytes);
+//        while(uploadTask.isInProgress()){
+//            System.out.println("waiting...");
+//        }
+//        System.out.println(uploadTask.isSuccessful());
 
 
-//        riversRef.putFile(file)
-//                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-//                    @Override
-//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-//                        // Get a URL to the uploaded content
-////                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception exception) {
-//                        // Handle unsuccessful uploads
-//                        // ...
-//                    }
-//                });
 
         System.out.println("BUTTON PRESSED");
+    }
+
+    private void updateUI(FirebaseUser user) {
+//        hideProgressDialog();
+//
+//        TextView idView = (TextView) findViewById(R.id.anonymous_status_id);
+//        TextView emailView = (TextView) findViewById(R.id.anonymous_status_email);
+//        boolean isSignedIn = (user != null);
+//
+//        // Status text
+//        if (isSignedIn) {
+//            idView.setText(getString(R.string.id_fmt, user.getUid()));
+//            emailView.setText(getString(R.string.email_fmt, user.getEmail()));
+//        } else {
+//            idView.setText(R.string.signed_out);
+//            emailView.setText(null);
+//        }
+//
+//        // Button visibility
+//        findViewById(R.id.button_anonymous_sign_in).setEnabled(!isSignedIn);
+//        findViewById(R.id.button_anonymous_sign_out).setEnabled(isSignedIn);
+//        findViewById(R.id.button_link_account).setEnabled(isSignedIn);
     }
 }
